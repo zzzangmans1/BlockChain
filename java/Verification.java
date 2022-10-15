591068e61004dbe826c029de78e684fd6de10909
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-10-15 18:03:45 +0900

    Update Verification.java

--- java/Verification.java
+++ java/Verification.java
@@ -18,19 +18,21 @@ public class Verification {
 	 *  @Param 복호화 할 키
 	 *  @Retun 검증 성공 여부
 	 */
-	public static boolean verfication(String signMessage, Key key) 
+	public boolean verification(String signMessage, Key key) 
 			throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, NoSuchProviderException {
+		Sha256 s = new Sha256();
 		String[] str = signMessage.split("\n");
-		System.out.println("Verfication.verfication Func : signMessage : " + signMessage);
-		System.out.println("Verfication.verfication Func : str[0] : " + str[0] );				// 온전한 메시지
-		System.out.println("Verfication.verfication Func : str[1] : " + str[1] );				// 키로 암호화 된 메시지
+		//System.out.println("Verfication.verfication Func : signMessage : " + signMessage);
+		//System.out.println("Verfication.verfication Func : str[0] : " + str[0] );				// 온전한 메시지
+		//System.out.println("Verfication.verfication Func : str[1] : " + str[1] );				// 키로 암호화 된 메시지
 		
 		String decSignMessage = Rsa.decryptRSA(str[1], key);									// 해시값 만 남음
-		System.out.println("Verfication.verfication Func : decSignMessage : " + decSignMessage);
-		String checkHash = Sha256.encrypt(str[0]);
-		System.out.println("Verfication.verfication Func : checkHash" + checkHash);
+		//System.out.println("Verfication.verfication Func : decSignMessage : " + decSignMessage);
+		String checkHash = s.encrypt(str[0]);
+		//System.out.println("Verfication.verfication Func : checkHash : " + checkHash);
 		
 		boolean b = checkHash.equals(decSignMessage);
+		//System.out.println("Verfication.verfication Func : bool " + b);
 		return b;
 	}
 	
@@ -40,13 +42,14 @@ public class Verification {
 	 *  @Param 암호화할 키
 	 *  @Return 서명
 	 */
-	public static String signature(String message, Key key) 
+	public String signature(String message, Key key) 
 			throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, NoSuchProviderException {
-		System.out.println("Verfication.signature Func : message : " + message);
-		String messageHash = Sha256.encrypt(message);
-		System.out.println("Verfication.signature Func : messageHash : " + messageHash);
+		Sha256 s = new Sha256();
+		//System.out.println("Verfication.signature Func : message : " + message);
+		String messageHash = s.encrypt(message);
+		//System.out.println("Verfication.signature Func : messageHash : " + messageHash);
 		String signMessage = message +"\n" + Rsa.encryptRSA(messageHash, key);
-		System.out.println("Verfication.signature Func : signMessage : "+ signMessage);
+		//System.out.println("Verfication.signature Func : signMessage : "+ signMessage);
 		return signMessage;
 	}
 }
