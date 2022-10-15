5bb88c6431942ed8627f5eed71c29c71a14f856e
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-10-15 18:03:33 +0900

    Update Transaction.java

--- java/Transaction.java
+++ java/Transaction.java
@@ -1,24 +1,42 @@
 package blockchain.midterm;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 // Transaction 클래스는 A라는 사람이 B라는 사람에게 코인을 얼마나 전송했는지에 대한 하나의 코인 거래 정보를 저장하는 역할 클래스
 public class Transaction {
-	String sender;
-	String receiver;
-	double amount;
 	
-	public String getSender() {
+	String signature;
+	PublicKey sender;
+	PublicKey receiver;
+	double amount;
+	Timestamp timestamp;
+	
+	public String getSignature() {
+		return signature;
+	}
+	public void setSignnature(String signature) {
+		this.signature = signature;
+	}
+	public PublicKey getSender() {
 		return sender;
 	}
-	public void setSender(String sender) {
+	public void setSender(PublicKey sender) {
 		this.sender = sender;
 	}
-	public String getReceiver() {
+	public PublicKey getReceiver() {
 		return receiver;
 	}
-	public void setReceiver(String receiver) {
+	public void setReceiver(PublicKey receiver) {
 		this.receiver = receiver;
 	}
 	public double getAmount() {
@@ -27,22 +45,34 @@ public class Transaction {
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
 	
-	public Transaction(String sender, String recevier, double amount)
+	public Transaction(Wallet wallet, PublicKey recevier, double amount, String timestamp) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, NoSuchProviderException
 	{
-		this.sender = sender;
+		
+		this.sender = wallet.getPublicKey();
 		this.receiver = recevier;
 		this.amount = amount;
+		this.timestamp = Timestamp.valueOf(timestamp);
+		this.signature = wallet.sign(getData());
 	}
 	
-	public String getDate() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
-		Date time = new Date();
-		String now = sdf.format(time);
-		return now;
+	
+	
+	// 서명 값을 포함한 단순 트랜잭션 정보를 반환
+	public String getInformation() throws NoSuchAlgorithmException {
+		Sha256 s = new Sha256();
+		return "<" + signature + ">\n" + s.encrypt(sender.toString()) + " -> " + s.encrypt(receiver.toString()) + " : " +amount + " 개 " + timestamp;
 	}
 	
-	public String getInformation() {
-		return "[" + getDate() + "]" + sender + "이(가) " +receiver + "에게" +amount + "개의 코인을 보냈습니다.";
+	// 서명 값을 제외한 단순 트랜젝션 정보를 반환
+	public String getData() throws NoSuchAlgorithmException {
+		Sha256 s = new Sha256();
+		return s.encrypt(sender.toString()) + " -> " + s.encrypt(receiver.toString()) + " : " +amount + " 개 " + timestamp;
 	}
 }
